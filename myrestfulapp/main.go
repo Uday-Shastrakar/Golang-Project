440056a88c524246b311131fa19dbcd320a60a31
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-12-08 01:14:50 +0530

    new go file

---
+++ myrestfulapp/main.go
@@ -0,0 +1,24 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+
+	http.Handle("/", r)
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"message": "Hello, World!"}`)
+}
